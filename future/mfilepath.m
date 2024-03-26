e039521142a08885a34dce8953471a1f53b645a5
Erik <erik_newton@hotmail.com>
2024-03-26 09:44:58 +0000

    Fix badly wrapped comment

--- future/mfilepath.m
+++ future/mfilepath.m
@@ -1,11 +1,11 @@
 function filepath = mfilepath()
-    %FILEPATH Path of currently executing MATLAB code file.
+    %mfilepath   Path of currently executing MATLAB code file.
     % milepath returns a character vector containing the path of the most
     % recently invoked MATLAB code file. When called from within a file,
     % it returns the path of that file.
     %    
-    % When called from the command line, mfilepath returns % an empty character
-    % vector.
+    % When called from the command line, mfilepath returns
+    % an empty character vector.
     %
     % See also mfilename.
     
