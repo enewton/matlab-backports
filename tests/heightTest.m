28eada94b86bae1c9db45f8732401314498c7b3e
Erik Newton <erik_newton@hotmail.com>
2024-05-17 20:15:30 +0100

    Correct height/width

---
+++ tests/heightTest.m
@@ -0,0 +1,3 @@
+assert(height(ones(2,3)) == 2);
+
+assert(height((table({'A';'B';'C'},[2; 3; 4]))) == 3);
---
+++ tests/widthTest.m
@@ -0,0 +1,3 @@
+assert(width(ones(2,3)) == 3);
+
+assert(width((table({'A';'B';'C'},[2; 3; 4]))) == 2);
